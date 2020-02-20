2eee36601745b4f4a1f69c4cca2ed42be1e73f32
Pavlo Taranov <taranov.pavel@gmail.com>
2020-02-20 12:51:15 +0100

    Crispy forms setup for bootstrap4

--- tigadev_challenge/settings.py
+++ tigadev_challenge/settings.py
@@ -123,3 +123,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
+#crispy forms settings
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
