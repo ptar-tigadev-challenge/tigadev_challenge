3cf90e62af38bd46a7f2bd90ee48add10b14241e
Pavlo Taranov <taranov.pavel@gmail.com>
2020-02-21 21:45:12 +0100

    Settings tuned

--- tigadev_challenge/settings.py
+++ tigadev_challenge/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3lfb2cz1w394(#wis*-@eep&8hc2satlu4_wttkv1s0cqzlv24'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['ptar-tigadev-challenge.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
